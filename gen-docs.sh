944f7b1e644afc7536092f42def5d5ab6a35a338
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2026-09-15 12:54:15 +0200

    Do not truncate long descriptions

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,2 +1,2 @@
 rm -rf docs/schema
-uv run linkml generate doc -d docs/schema --subfolder-type-separation --no-include-top-level-diagram --no-metadata linkml/mastr.yml
+uv run linkml generate doc -d docs/schema --subfolder-type-separation --no-include-top-level-diagram --no-metadata --truncate-descriptions False linkml/mastr.yml
